17a195c38438ae8b71cf5539f97ec44cdcec98b9
ellaayu <ella.ayuningtyas.eru@gmail.com>
2017-01-15 15:56:14 +0700

    Update Home.php

--- controllers/Home.php
+++ controllers/Home.php
@@ -14,13 +14,25 @@ class Home extends CI_Controller {
         $isi['content'] = 'tampilan_content';
         $isi['judul'] = 'Beranda';
         $isi['sub_judul'] = 'Selamat datang di Sistem Manajemen Kontrakan';
-		$this->load->view('tampilan_home',$isi);  
+	$isi['listselasa'] = $this->Model_data_tugas->selasa();
+        $isi['list'] = $this->Model_data_tugas->piket();
+        $isi['listrabu'] = $this->Model_data_tugas->rabu();
+        $isi['listkamis'] = $this->Model_data_tugas->kamis();
+        $isi['listjumat'] = $this->Model_data_tugas->jumat();
+        $isi['listsabtu'] = $this->Model_data_tugas->sabtu();
+        $isi['listminggu'] = $this->Model_data_tugas->minggu();
+        
+		$this->load->view('tampilan_home',$isi); 
         
 	}
 
+    
     public function logout()
     {
         $this->session->sess_destroy();
         redirect('login');
     }
+    
+    
+   
 }
